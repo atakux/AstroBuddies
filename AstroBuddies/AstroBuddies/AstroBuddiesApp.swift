d5e950313a4b861481e1658cd52cc19d2a9f2c0b
Angela DeLeo <rockyangela5@gmail.com>
2023-10-25 21:25:52 -0700

    created Xcode project

---
+++ AstroBuddies/AstroBuddies/AstroBuddiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstroBuddiesApp.swift
+//  AstroBuddies
+//
+//  Created by ataku x on 10/25/23.
+//
+
+import SwiftUI
+
+@main
+struct AstroBuddiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AstroBuddies/AstroBuddies/ContentView.swift
@@ -0,0 +1,10 @@
+//
+//  ContentView.swift
+//  AstroBuddies
+//
+//  Created by ataku x on 10/25/23.
+//
+
+import SwiftUI
+
+
